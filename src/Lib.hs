67ffb79030dc2fc53a92b81e984b1268fba10207
BryanCruz <bryan.bialokur@gmail.com>
2018-12-01 13:43:53 -0200

    feat: add puzzle - Day 1: Chronal Calibration

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( chronalCalibration
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Day 1
+
+chronalCalibration :: IO ()
+chronalCalibration = do
+    content <- getContents
+    let numbers = map getInt (lines content)
+        result  = sum numbers
+    putStrLn $ show result
+
+-- Util
+getInt :: [Char] -> Integer
+getInt ('-':xs) = -(getInt xs)
+getInt ('+':xs) = getInt xs
+getInt       xs = read xs :: Integer
+
