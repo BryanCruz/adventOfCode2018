e2bcc83f66a5d021890edad660671daceb207058
BryanCruz <bryan.bialokur@gmail.com>
2018-12-01 16:40:44 -0200

    feat: add puzzle - Day 1: Chronal Calibration part 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,43 @@
 module Lib
-    ( chronalCalibration
+    ( chronalCalibration1,
+      chronalCalibration2
     ) where
 
--- Day 1
+import Debug.Trace
 
-chronalCalibration :: IO ()
-chronalCalibration = do
+-- Day 1
+--
+--     Part 1
+chronalCalibration1 :: IO ()
+chronalCalibration1 = do
     content <- getContents
-    let numbers = map getInt (lines content)
+    let numbers = getNumbers content
         result  = sum numbers
     putStrLn $ show result
+
+--     Part 2
+chronalCalibration2 :: IO ()
+chronalCalibration2 = do
+    content <- getContents
+    let numbers = getNumbers content
+        result  = repeatedFrequency [0] (concat $ repeat numbers)
+    putStrLn $ show result
+
+    where
+    repeatedFrequency alreadySeen numbers
+        | currFreq `elem` alreadySeen = currFreq
+        | otherwise                   = repeatedFrequency nextAlreadySeen nextNumbers
+        where
+            currValue       = head numbers
+            currFreq        = head alreadySeen + currValue
+            nextNumbers     = tail numbers
+            nextAlreadySeen = currFreq : alreadySeen
 
 -- Util
 getInt :: [Char] -> Integer
 getInt ('-':xs) = -(getInt xs)
 getInt ('+':xs) = getInt xs
-getInt       xs = read xs :: Integer
+getInt      xs  = read xs :: Integer
+
+getNumbers content = map getInt (lines content)
 
