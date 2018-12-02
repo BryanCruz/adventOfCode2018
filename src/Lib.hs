d2f6b30b596b688a2ab0eda77e96499549f22e60
BryanCruz <bryan.bialokur@gmail.com>
2018-12-02 10:45:04 -0200

    feat: add puzzle - Day 2: Inventory Management System, part 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
     ( chronalCalibration1,
-      chronalCalibration2
+      chronalCalibration2,
+      inventoryManagementSystem1,
+      inventoryManagementSystem2
     ) where
 
 import Debug.Trace
@@ -32,6 +34,37 @@ chronalCalibration2 = do
             currFreq        = head alreadySeen + currValue
             nextNumbers     = tail numbers
             nextAlreadySeen = currFreq : alreadySeen
+-- Day 2
+--
+--     Part 1
+inventoryManagementSystem1 :: IO ()
+inventoryManagementSystem1 = do
+    content <- getContents
+    let boxes  = lines content
+        aux    = [count $ quicksort box | box <- boxes]
+        aux2   = length $ filter (2 `elem`) aux
+        aux3   = length $ filter (3 `elem`) aux
+        result = aux2 * aux3
+    putStrLn $ show result
+
+    where
+        count :: [Char] -> [Integer]
+        count []     = []
+        count (x:xs) = (countX+1) : (count remainingList)
+            where
+                (countX, remainingList) = count' x xs
+
+        count' :: Char -> [Char] -> (Integer, [Char])
+        count' _ []      = (0, [])
+        count' y (x:xs)
+            | y == x    = (1+nextCount, remainingList)
+            | otherwise = (0, (x:xs))
+            where
+                (nextCount, remainingList) = count' x xs
+
+--    Part 2
+inventoryManagementSystem2 :: IO ()
+inventoryManagementSystem2 = undefined
 
 -- Util
 getInt :: [Char] -> Integer
@@ -41,3 +74,9 @@ getInt      xs  = read xs :: Integer
 
 getNumbers content = map getInt (lines content)
 
+quicksort :: Ord a => [a] -> [a]
+quicksort []     = []
+quicksort (x:xs) = minors ++ [x] ++ majors
+    where
+        minors = quicksort [a | a <- xs, a <= x]
+        majors = quicksort [a | a <- xs, a  > x]
