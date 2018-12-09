e11f89c424fdc63c200aea7f6cfc134e54b4c0ae
BryanCruz <bryan.bialokur@gmail.com>
2018-12-09 13:05:20 -0200

    feat: add Info part of Entry

--- src/Day4Part1.hs
+++ src/Day4Part1.hs
@@ -4,6 +4,7 @@ module Day4Part1
 import Util
 import Date
 import Debug.Trace
+import qualified Data.Map as Map
 
 -- Day 4 - Part 1
 -- Repose Record
@@ -13,15 +14,36 @@ sol = do
   let result = getResult content
   putStrLn $ show result
 
+data Entry = Entry { date :: Date, info :: Info Int} deriving (Show, Read)
+
+data Info s = BeginsShift s | Sleep | WakesUp deriving (Show, Read)
+
+instance Eq Entry where
+  (==) e1 e2 
+    = (==) (date e1) (date e2)
+
+instance Ord Entry where
+  compare e1 e2
+    = compare (date e1) (date e2)
+
 getResult content = do
-  let entries = map getEntry $ lines content
-      foo     = trace "aa" "bbb" 
-  entries !! 0
+  let entries = quicksort $ map getEntry $ lines content
+  entries
 
-getEntry :: [Char] -> (Date, [Char]) 
-getEntry xs = (date, info)
+getEntry :: [Char] -> Entry
+getEntry xs = Entry date info
   where
-    date = parseDate $ take 18 xs
-    info  = drop 19 xs
+    date    = parseDate $ take 18 xs
+    info    = parseInfo $ drop 19 xs
 
-
+parseInfo :: [Char] -> Info Int
+parseInfo "wakes up"     = WakesUp
+parseInfo "falls asleep" = Sleep
+parseInfo ss             = BeginsShift guard
+  where
+    guard               = read (nextInt ss) :: Int
+    nextInt (' ':'b':_) = []
+    nextInt (x:xs)
+      | isCharInt x     = x : nextInt xs
+      | otherwise       = nextInt xs
+    isCharInt x         = x >= '0' && x <= '9'
