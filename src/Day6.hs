7eb2275e558ee6041deb8ae49d38701cdaa42f2f
BryanCruz <bryan.bialokur@gmail.com>
2018-12-12 10:29:27 -0200

    refact: use Int tuples instead of a new type for Points, for simplicity

--- src/Day6.hs
+++ src/Day6.hs
@@ -13,60 +13,43 @@ sol = do
   putStrLn result1
   putStrLn result2
 
-getResults content = do
-  let points         = map parsePoint $ lines content
-      inferiorLimitP = Point (minimumX points) (minimumY points)
-      superiorLimitP = Point (maximumX points) (maximumY points)
-      result1 = show 1
-      result2 = show 2
+type Point = (Int,Int)
 
-  (result1, result2)
-
-manhattanDistance :: Point -> Point-> Int
-manhattanDistance (Point x1 y1) (Point x2 y2)
-  = abs (x1 - x2) + abs (y1 - y2)
-
-minimumX :: [Point] -> Int
-minimumX points = minimum [x | (Point x _) <- points]
-
-minimumY :: [Point] -> Int
-minimumY points = minimum [y | (Point _ y) <- points]
-
-maximumX :: [Point] -> Int
-maximumX points = maximum [x | (Point x _) <- points]
-
-maximumY :: [Point] -> Int
-maximumY points = maximum [y | (Point _ y) <- points]
+sumPoints :: Point -> Point -> Point
+sumPoints (x1, y1) (x2, y2)
+  = (x1+x2, y1+y2)
 
 -- Input points are like "152, 292"
 parsePoint :: [Char] -> Point
 parsePoint cs
-  = Point x' y'
+  = (x', y')
   where
     x  = takeWhile (/= ',')  cs
     y  = drop (2 + length x) cs
     x' = read x :: Int
     y' = read y :: Int
 
-data Point = Point Int Int deriving (Show, Eq)
 
-instance Num Point where
-  (+) (Point x1 y1) (Point x2 y2)
-    = Point (x1 + x2) (y1 + y2)
+getResults content = do
+  let points  = map parsePoint $ lines content
+      result1 = show 1
+      result2 = show 2
 
-  (*) (Point x1 y1) (Point x2 y2)
-    = Point (x1 * x2) (y1 * y2)
+  (result1, result2)
 
-  abs (Point x y)    = Point (abs x) (abs y)
+manhattanDistance :: Point -> Point -> Int
+manhattanDistance (x1, y1) (x2, y2)
+  = abs (x1 - x2) + abs (y1 - y2)
 
-  signum (Point x y) = Point (signum x) (signum y)
+minimumX :: [Point] -> Int
+minimumX points = minimum [x | (x, _) <- points]
 
-  fromInteger a      = Point (fromInteger a) (fromInteger a)
+minimumY :: [Point] -> Int
+minimumY points = minimum [y | (_, y) <- points]
 
-  negate (Point x y) = Point (-x) (-y)
+maximumX :: [Point] -> Int
+maximumX points = maximum [x | (x, _) <- points]
 
--- The Ord is implemented like this to help 
--- us to categorize the closest points to another
-instance Ord Point where
-  compare (Point x1 y1) (Point x2 y2)
-    = compare (x1+y1) (x2+y2)
+maximumY :: [Point] -> Int
+maximumY points = maximum [y | (_, y) <- points]
+
