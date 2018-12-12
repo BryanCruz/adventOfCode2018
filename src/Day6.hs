15cfc61fc588b8e745993b3c00a028497e989b0e
BryanCruz <bryan.bialokur@gmail.com>
2018-12-12 12:02:07 -0200

    feat: solve puzzle - Day 6: Chronal Coordinates, Part 1

--- src/Day6.hs
+++ src/Day6.hs
@@ -31,25 +31,52 @@ parsePoint cs
 
 
 getResults content = do
-  let points  = map parsePoint $ lines content
-      result1 = show 1
-      result2 = show 2
+  let points   = map parsePoint $ lines content
 
+      minimumX = minimum [x | (x, _) <- points]
+      minimumY = minimum [y | (_, y) <- points]
+      maximumX = maximum [x | (x, _) <- points]
+      maximumY = maximum [y | (_, y) <- points]
+      
+      sizeX    = maximumX - minimumX
+      sizeY    = maximumY - minimumY
+
+      allPoints = [(x, y) | x <- xRange, y <- yRange]
+        where
+          xRange = [minimumX..maximumX]
+          yRange = [minimumY..maximumY]
+   
+      closestPoints  = [(p0, closestPoint p0) | p0 <- allPoints]
+        where
+          closestPoint p0 = do
+            let allDistances        = [(pf, manhattanDistance p0 pf) | pf <- points]
+                (potentialClosestPoint, minorDistance) = minimumSnd allDistances
+
+            if   minorDistance `appearsOnce` [snd t | t <- allDistances]
+            then Just potentialClosestPoint
+            else Nothing
+
+      -- Closest to Edge Points have infinite closest points, so we do not count them
+      edgePoints          = [(x, y) | (x, y) <- allPoints, x `elem` [minimumX, maximumX] || y `elem` [minimumY, maximumY]]
+      closestToEdgePoints = [pf | (p0, pf) <- closestPoints, pf /= Nothing, p0 `elem` edgePoints]
+      largestArea         = [count p closestPoints | p <- points, not $ Just p `elem` closestToEdgePoints]
+        where
+          count a xs = length [1 | (_, x) <- xs, x == Just a]
+
+    
+      result1 = show $ maximum largestArea
+      result2 = show largestArea
   (result1, result2)
 
 manhattanDistance :: Point -> Point -> Int
 manhattanDistance (x1, y1) (x2, y2)
   = abs (x1 - x2) + abs (y1 - y2)
 
-minimumX :: [Point] -> Int
-minimumX points = minimum [x | (x, _) <- points]
+minimumSnd []     = error "Empty list"
+minimumSnd [t]    = t
+minimumSnd (t:ts) = if v < v' then (k, v) else (k', v')
+  where
+    (k,   v)  = t
+    (k',  v') = minimumSnd ts
 
-minimumY :: [Point] -> Int
-minimumY points = minimum [y | (_, y) <- points]
-
-maximumX :: [Point] -> Int
-maximumX points = maximum [x | (x, _) <- points]
-
-maximumY :: [Point] -> Int
-maximumY points = maximum [y | (_, y) <- points]
-
+appearsOnce a xs = length [x | x <- xs, x == a] == 1
