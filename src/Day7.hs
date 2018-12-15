1696f59fdbe7dae132c206574b79c71378a744a1
BryanCruz <bryan.bialokur@gmail.com>
2018-12-15 13:54:01 -0200

    feat: solve puzzle - Day 7: The Sum of Its Parts, Part 2

--- src/Day7.hs
+++ src/Day7.hs
@@ -5,7 +5,6 @@ import Util
 import qualified Data.Set  as Set
 import qualified Data.Char as Char
 import qualified Data.HashMap.Lazy as Map
-import Debug.Trace
 
 -- Day 7 - Part 1 and 2
 -- The Sum of Its Parts
@@ -58,24 +57,56 @@ getResult1 content = do
 charToInt :: Char -> Int
 charToInt c = Char.ord c - Char.ord 'A' + 1
 
-workers = 5
-
-stepTime = (+60) . charToInt
-
-createGraph :: [(Char, Char)] -> Map.HashMap Char (Set.Set Char)
+-- A Graph that maps each Task to their Dependencies
+type Graph = Map.HashMap Char [Char]
+createGraph :: [(Char, Char)] -> Graph
 createGraph = foldr insertVertex Map.empty
   where
-    insertVertex (vertex1, vertex2) = Map.insertWith Set.union vertex2 (Set.singleton vertex1)
+    insertVertex (vertex1, vertex2) = Map.insertWith (++) vertex2 [vertex1]
 
-totalTime 
+nWorkers  = 5
+
+taskTime = (+60) . charToInt
+
+data Job   = Job   {task     :: Char, finalTime :: Int  } deriving (Show)
+data State = State {currTime :: Int,  jobs      :: [Job]} deriving (Show)
+
+updateState :: State -> Graph -> State
+updateState s g = finalState
+  where
+    finalizedTasks = [tk | (Job tk tm) <- (jobs s), tm == currTime s]
+    updatedGraph   = Map.map (remove finalizedTasks) g
+      where
+        remove ft []     = []
+        remove ft (v:vs) = if v `elem` ft then vs' else (v:vs')
+          where
+            vs' = remove ft vs
+
+    remainingJobs = [j | j <- (jobs s), not $ (task j) `elem` finalizedTasks]
+
+    possibleTasks = quicksort [t | (t, _) <- (Map.toList $ Map.filter (== "0") updatedGraph)]
+    possibleJobs  = [j | t <- possibleTasks, let j = Job t (currTime s + taskTime t)]
+
+    updatedJobs  = take nWorkers $ remainingJobs ++ possibleJobs
+
+    updatedState = State (currTime s + 1) updatedJobs
+   
+    finalState   = if   length updatedJobs == 0 
+                   then updatedState
+                   else updateState updatedState (Map.filterWithKey (\k _ -> not $ k `elem` updatedTasks) updatedGraph)
+      where
+        updatedTasks = [tk | (Job tk _) <- updatedJobs]
+    
 
 getResult2 content = do
   let dependencies     = map parseDependency $ lines content
 
-      wardDependencies = Set.toList $ Set.fromList [ ('0', x) | (x, _) <- dependencies]
+      wardDependencies = Set.toList $ Set.fromList $ [('0', x) | (x, _) <- dependencies] ++ [('0', y) | (_, y) <- dependencies]
 
       dependencyTree   = createGraph $ dependencies ++ wardDependencies
 
-      result2          = totalTime dependencyTree
-     
+      totalTime        = currTime $ updateState s0 dependencyTree
+        where s0 = State (-1) []
+
+      result2          = show $ totalTime
   result2 
